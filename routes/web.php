cc3e996c5da0ab3671b2f61675c09ac3e5288c0e
ririnrosdyanti05 <sayarinbhyy@gmail.com>
2025-01-17 11:41:06 +0700

    Roles Permissions

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware'=> ['auth']], function() {
+    Route::resource('roles', UserController::class);
+    Route::resource('user', UserController::class);
+});
